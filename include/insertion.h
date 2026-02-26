b45b849b0a5fb16b1ad4d48befa7b05744eb4a65
fgrdiogo <diogogomesf5@gmail.com>
2026-02-26 20:10:35 -0300

    Adicionado o insertion para as listas dinâmicas e encadeadas

--- include/insertion.h
+++ include/insertion.h
@@ -1,12 +1,26 @@
 #ifndef INSERTION_H
 #define INSERTION_H
 
+// Define Aluno aqui (somente se ainda não tiver sido definido)
+#ifndef ALUNO_TYPE_DEFINED
+#define ALUNO_TYPE_DEFINED
 typedef struct {
     int id;
     float nota;
 } Aluno;
+#endif
 
-// ordena vetor de alunos por nota (crescente)
+// Forward declarations para evitar include circular
+typedef struct ListaDinamica ListaDinamica;
+typedef struct ListaEstatica ListaEstatica;
+
+// 1) Insertion Sort em vetor (ordena por nota crescente)
 void insertion_sort(Aluno v[], int n);
+
+// 2) Insertion Sort na lista dinâmica (in-place, por nota crescente)
+void lista_dinamica_insertion_sort(ListaDinamica *lista);
+
+// 3) Insertion Sort na lista estática (in-place, por nota crescente)
+void lista_estatica_insertion_sort(ListaEstatica *l);
 
 #endif
